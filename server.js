1eaec4c8a75da6a86a5c7e1620c89526fc87de1d
Douglas Gardner <douglas@chippy.ch>
2013-08-26 08:11:34 +0000

    refactor

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var log = require('./log');
-var fs = require('fs');
-var md = require('marked');
 var config = require('./package.json').config;
-var moment = require('moment');
 var express = require('express');
+var fs = require('fs');
 var jade = require('jade');
+var log = require('./log');
+var md = require('marked');
+var moment = require('moment');
 var rss = require('rss');
 
 var app = express();
+
 app.listen(config.web.port);
 log.info("Listening on port " + config.web.port);
 
@@ -16,57 +17,47 @@ log.info("Starting up!");
 var postsdir = 'posts';
 
 app.get("/", function(req, res){
-    var posts = [];
-    fs.readdir(postsdir,function(err, files){
-        if(err){
-            log.error("Unable to read posts directory!");
-        } else {
-            files.reverse();
-            files.forEach(function(file){
-                if((file.substring(file.lastIndexOf('.')+1)) == "md"){
-                    content = md(fs.readFileSync(postsdir+'/'+file,{'encoding':'utf-8'}));
-                    var stat = fs.statSync(postsdir+'/'+file);
-                    log.info("Rendering " + file);
-                    posts.push({
-                        content: content,
-                        date: moment(stat.mtime).fromNow(),
-                        vdate: stat.mtime
-                    });
-                } else {
-                    log.info("Skipping " + file + " (not Markdown)");
-                }
-            });
-        }
-        res.send(jade.renderFile("views/main.jade",{posts:posts,config:config}));
-    });
+    var posts = getPosts();
+    res.send(jade.renderFile("views/main.jade",{posts:posts,config:config}));
 });
 
 app.get("/rss", function(req, res){
+    log.info("Rendering RSS");
+    var posts = getPosts();
     var feed = new rss({
         title: config.web.header,
         description: config.web.description,
         feed_url: config.web.url + '/rss',
         site_url: config.web.url
     });
-
-    fs.readdir(postsdir,function(err, files){
-        if(err){
-            log.error("Unable to read posts directory!");
-        } else {
-            files.reverse();
-            files.forEach(function(file){
-                if((file.substring(file.lastIndexOf('.')+1)) == "md"){
-                    content = md(fs.readFileSync(postsdir+'/'+file,{'encoding':'utf-8'}));
-                    var stat = fs.statSync(postsdir+'/'+file);
-                    feed.item({
-                        date: stat.mtime,
-                        description: content
-                    });
-                } else {
-                    log.info("Skipping " + file + " (not Markdown)");
-                }
-            });
-        }
-        res.send(feed.xml(true));
+    posts.forEach(function(post){
+        feed.item({
+            date: post.date,
+            description: post.content,
+            title: post.content.substr(0, post.content.indexOf('\n')).replace(/<(?:.|\n)*?>/gm, '')
+        });
     });
+    res.send(feed.xml(true));
 });
+
+function getPosts(){
+    var posts = [];
+    var files = fs.readdirSync(postsdir);
+    files.reverse();
+    files.forEach(function(file){
+        if((file.substring(file.lastIndexOf('.')+1)) == "md"){
+            content = md(fs.readFileSync(postsdir+'/'+file,{'encoding':'utf-8'}));
+            var stat = fs.statSync(postsdir+'/'+file);
+            log.info("Rendering " + file);
+            var post = {
+                content: content,
+                date: moment(stat.mtime).fromNow(),
+                vdate: stat.mtime
+            };
+            posts.push(post);
+        } else {
+            log.info("Skipping " + file + " (not Markdown)");
+        }
+    });
+    return posts;
+}
