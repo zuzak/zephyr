2ffe43ded9729ecc7d7f95d1e36a82df19588b7d
Douglas Gardner <douglas@chippy.ch>
2013-08-26 07:44:27 +0000

    add rss

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var config = require('./package.json').config;
 var moment = require('moment');
 var express = require('express');
 var jade = require('jade');
+var rss = require('rss');
 
 var app = express();
 app.listen(config.web.port);
@@ -37,5 +38,35 @@ app.get("/", function(req, res){
             });
         }
         res.send(jade.renderFile("views/main.jade",{posts:posts,config:config}));
+    });
+});
+
+app.get("/rss", function(req, res){
+    var feed = new rss({
+        title: config.web.header,
+        description: config.web.description,
+        feed_url: config.web.url + '/rss',
+        site_url: config.web.url
+    });
+
+    fs.readdir(postsdir,function(err, files){
+        if(err){
+            log.error("Unable to read posts directory!");
+        } else {
+            files.reverse();
+            files.forEach(function(file){
+                if((file.substring(file.lastIndexOf('.')+1)) == "md"){
+                    content = md(fs.readFileSync(postsdir+'/'+file,{'encoding':'utf-8'}));
+                    var stat = fs.statSync(postsdir+'/'+file);
+                    feed.item({
+                        date: stat.mtime,
+                        description: content
+                    });
+                } else {
+                    log.info("Skipping " + file + " (not Markdown)");
+                }
+            });
+        }
+        res.send(feed.xml(true));
     });
 });
